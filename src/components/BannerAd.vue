537688a0a7824f31082ac60b0b7944d4196ae218
josipa-simanovic <josipa@dikobraz.hr>
2025-07-01 11:53:45 +0200

    banner ad align center

--- src/components/BannerAd.vue
+++ src/components/BannerAd.vue
@@ -4,7 +4,7 @@
         id="banner-ad-canvas"
         width="320"
         height="50"
-        style="border:1px solid #8be9ff; display: block;background-color: #8be9ff;"
+        style="border:1px solid #8be9ff; display: block;background-color: #8be9ff;margin:auto"
       >
         Your browser does not support canvas.
       </canvas>
