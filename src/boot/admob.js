4ddab563a88dcee0a5c24a984ce512cba0965972
kv-iqsay <KV@int.iqsay.com>
2025-07-08 21:16:25 +0200

    ads update

--- src/boot/admob.js
+++ src/boot/admob.js
@@ -1,11 +1,27 @@
 import { AdMob, BannerAdSize, BannerAdPosition } from '@capacitor-community/admob'
 
-AdMob.initialize()
+async function showAds() {
+  await AdMob.initialize()
 
-AdMob.showBanner({
-  adId: 'ca-app-pub-3940256099942544/6300978111', // Test ID
-  adSize: BannerAdSize.BANNER,
-  position: BannerAdPosition.TOP_CENTER,
-  margin: 0,
-  isTesting: true
-})
+  // Show banner ad
+  await AdMob.showBanner({
+    adId: 'ca-app-pub-3940256099942544/6300978111', // Test banner ID
+    adSize: BannerAdSize.BANNER,
+    position: BannerAdPosition.TOP_CENTER,
+    margin: 0,
+    isTesting: true
+  })
+
+  // Prepare interstitial ad
+  await AdMob.prepareInterstitial({
+    adId: 'ca-app-pub-3940256099942544/1033173712', // Test interstitial ID
+    isTesting: true
+  })
+
+  // Show interstitial after 5 seconds
+  setTimeout(async () => {
+    await AdMob.showInterstitial()
+  }, 5000)
+}
+
+document.addEventListener('deviceready', showAds)
