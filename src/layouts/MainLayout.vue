0a5fe7fedb3e6d85603cbdf124dd7fd7fd5b888a
josipa-simanovic <josipa@dikobraz.hr>
2025-08-19 09:29:37 +0200

    logo down, bottom nav up

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,57 +1,42 @@
 <template>
   <q-layout view="hHh lpR fff">
-<div class="row">
-  
-  <div class="col-12 justify-center items-center text-center">
-    <router-link to="/"> <q-img src="horizontal-logo.png" width="200px" class="q-mt-lg q-mx-auto block"></q-img></router-link>
-  </div>
-</div>
- 
+    <div class="row">
 
-   
+      <div class="col-12 justify-center items-center text-center q-mt-lg">
+        <router-link to="/"> <q-img src="horizontal-logo.png" width="200px"
+            class="q-mt-xl q-mx-auto block"></q-img></router-link>
+      </div>
+    </div>
+
+
+
 
     <q-page-container class="q-pb-xl q-mb-lg">
       <router-view />
     </q-page-container>
 
-    <div class="bottom-toolbar">
-      <button
-        class="toolbar-btn"
-        :class="{ active: activeTab === 0 }"
-        @click="goTo('/')"
-        aria-label="Home"
-      >
+    <div class="bottom-toolbar q-pb-xl">
+      <button class="toolbar-btn" :class="{ active: activeTab === 0 }" @click="goTo('/')" aria-label="Home">
         <q-icon name="home" />
-        <span>{{$t('home')}}</span>
+        <span>{{ $t('home') }}</span>
       </button>
 
-      <button
-        class="toolbar-btn"
-        :class="{ active: activeTab === 1 }"
-        @click="goTo('/category-all')"
-        aria-label="Categories"
-      >
+      <button class="toolbar-btn" :class="{ active: activeTab === 1 }" @click="goTo('/category-all')"
+        aria-label="Categories">
         <q-icon name="category" />
-        <span>{{$t('Categories')}}</span>
+        <span>{{ $t('Categories') }}</span>
       </button>
- <button
-        class="toolbar-btn"
-        :class="{ active: activeTab === 3 }"
-        @click="goTo('/game')"
-        aria-label="game"
-      >
-<div class="custom-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M182.15-140 140-182.15 445.23-488l-174.84-43.08L453-644.46 437.16-860l164.76 139.08L801-802l-80.08 199.69L860-438.16 644.46-453l-114 182.61L487-445.23 182.15-140Zm23.62-523.85L140-729.62l65.77-65.76 65.77 65.76-65.77 65.77ZM555-422.61l57.23-93.24 109.54 8.16L651-591.77l41.16-101.39L590.77-652l-84.08-70.16 8.16 108.93L422-555.38l106.15 26.23L555-422.61ZM729.62-140l-65.77-65.77 65.77-65.77 65.76 65.77L729.62-140ZM572.08-572.69Z"/></svg>
-     </div>   <span>{{$t('Game')}}</span>
+      <button class="toolbar-btn" :class="{ active: activeTab === 3 }" @click="goTo('/game')" aria-label="game">
+        <div class="custom-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <path
+              d="M182.15-140 140-182.15 445.23-488l-174.84-43.08L453-644.46 437.16-860l164.76 139.08L801-802l-80.08 199.69L860-438.16 644.46-453l-114 182.61L487-445.23 182.15-140Zm23.62-523.85L140-729.62l65.77-65.76 65.77 65.76-65.77 65.77ZM555-422.61l57.23-93.24 109.54 8.16L651-591.77l41.16-101.39L590.77-652l-84.08-70.16 8.16 108.93L422-555.38l106.15 26.23L555-422.61ZM729.62-140l-65.77-65.77 65.77-65.77 65.76 65.77L729.62-140ZM572.08-572.69Z" />
+          </svg>
+        </div> <span>{{ $t('Game') }}</span>
       </button>
-      <button
-        class="toolbar-btn"
-        :class="{ active: activeTab === 2 }"
-        @click="goTo('/profile')"
-        aria-label="Profile"
-      >
+      <button class="toolbar-btn" :class="{ active: activeTab === 2 }" @click="goTo('/profile')" aria-label="Profile">
         <q-icon name="person" />
-        <span>{{$t('profile')}}</span>
+        <span>{{ $t('profile') }}</span>
       </button>
     </div>
   </q-layout>
@@ -80,7 +65,7 @@ watch(
       activeTab.value = 0
     } else if (newPath.startsWith('/profile') || newPath.startsWith('/terms') || newPath.startsWith('/instructions')) {
       activeTab.value = 2
-    }else if (newPath === '/game') {
+    } else if (newPath === '/game') {
       activeTab.value = 3
     } else {
       activeTab.value = -1
@@ -90,8 +75,8 @@ watch(
 )
 </script>
 <style>
-.q-page{
-min-height:calc(100vh - 90px -90px)!important
+.q-page {
+  min-height: calc(100vh - 90px -90px) !important
 }
 </style>
 <style scoped>
@@ -99,7 +84,8 @@ min-height:calc(100vh - 90px -90px)!important
   position: fixed;
   top: 8px;
   left: 8px;
-  z-index: 4001; /*.qdrawer po defaultu ima zindex 1000, a na mobile 3000 */
+  z-index: 4001;
+  /*.qdrawer po defaultu ima zindex 1000, a na mobile 3000 */
 }
 
 .bottom-toolbar {
@@ -112,7 +98,7 @@ min-height:calc(100vh - 90px -90px)!important
   display: flex;
   justify-content: space-around;
   align-items: center;
- padding: 10px 5px;
+  padding: 10px 5px;
   box-shadow: 0 -2px 6px rgba(0, 0, 0, 0.1);
   border-top: 1px solid #ddd;
   z-index: 5000;
@@ -138,9 +124,11 @@ min-height:calc(100vh - 90px -90px)!important
 
   justify-content: center;
 }
-.q-icon{
+
+.q-icon {
   width: 1em;
 }
+
 .toolbar-btn .q-icon {
   font-size: 20px;
   width: 40px;
@@ -149,6 +137,7 @@ min-height:calc(100vh - 90px -90px)!important
   margin-top: 10px;
   padding: 0 1px 2px;
 }
+
 .custom-icon {
   width: 40px;
   height: 40px;
@@ -180,11 +169,11 @@ min-height:calc(100vh - 90px -90px)!important
   color: #818e9b;
 }
 
-.toolbar-btn.active .q-icon{
+.toolbar-btn.active .q-icon {
   color: #fff;
   background-color: #ff8a25;
   border-radius: 50%;
- 
+
 }
 
 
@@ -195,5 +184,4 @@ min-height:calc(100vh - 90px -90px)!important
 h6 {
   margin: 12px;
 }
-
 </style>
